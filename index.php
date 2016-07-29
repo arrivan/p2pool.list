c2932111ef8134eea7c686ccfd606d308b4e1ade
lepus <poiuty@lepus.su>
2016-07-29 18:29:15 +0300

    fix

--- index.php
+++ index.php
@@ -20,25 +20,16 @@ function ghash($globall_hashrate){
 	return round($hash_display, 2).$hash_string;
 }
 
-$json = json_decode(file_get_contents("http://eu.p2pool.pl:9171/global_stats"));
+$ctx = stream_context_create(['http'=> ['timeout' => 1]]);
+
+$json = json_decode(file_get_contents("http://eu.p2pool.ovh:9171/global_stats", false, $ctx));
 $vtc_hashrate = ghash($json->{"pool_hash_rate"});
 
-$json = json_decode(file_get_contents("http://eu.p2pool.pl:7903/global_stats"));
+$json = json_decode(file_get_contents("http://eu.p2pool.ovh:7903/global_stats", false, $ctx));
 $drk_hashrate = ghash($json->{"pool_hash_rate"});
 
-$json = json_decode(file_get_contents("http://eu.p2pool.pl:19327/global_stats"));
-$ftc_hashrate = ghash($json->{"pool_hash_rate"});
-
-$json = json_decode(file_get_contents("http://eu.p2pool.pl:9332/global_stats"));
+$json = json_decode(file_get_contents("http://eu.p2pool.ovh:9332/global_stats", false, $ctx));
 $btc_hashrate = ghash($json->{"pool_hash_rate"});
-
-
-$vtc_price = file_get_contents("http://dash.org.ru/price.php?name=VTC");
-$drk_price = file_get_contents("http://dash.org.ru/price.php?name=DRK");
-$ftc_price = file_get_contents("http://dash.org.ru/price.php?name=FTC");
-$btc_price = file_get_contents("http://dash.org.ru/price.php?name=BTC");
-// test git commit from linux
-// test ssh upload commit
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,37 +40,26 @@ $btc_price = file_get_contents("http://dash.org.ru/price.php?name=BTC");
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="shortcut icon" href="./favicon.ico">
-	
-    <title>P2Pool: Bitcoin, Vertcoin, DASH, Darkcoin, Feathercoin</title>
+    <title>P2Pool: Bitcoin, Vertcoin, DASH, DASH</title>
 	<meta name="description" content="p2pool vertcoin, dash, bitcoin, darkcoin, feathercoin" />
 	<meta name="keywords" content="p2pool, dash, bitcoin, vertcoin, darkcoin, feathercoin" />
-	<link rel="stylesheet" href="//p2pool.pl/css/bootstrap.css">
-	<link rel="stylesheet" href="//p2pool.pl/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="//p2pool.ovh/css/bootstrap.css">
+	<link rel="stylesheet" href="//p2pool.ovh/css/bootstrap-theme.min.css">
 	<link rel="stylesheet" type="text/css" href="css/alertify.core.css"/>
 	<link rel="stylesheet" type="text/css" href="css/alertify.bootstrap.css"/>
-	<link href="//p2pool.pl/css/justified-nav.css" rel="stylesheet">
+	<link href="//p2pool.ovh/css/justified-nav.css" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="//p2pool.pl/js/bootstrap.min.js"></script>
-	<script src="//p2pool.pl/js/alertify.js"></script>
-	<!--<style>.dropdown:hover > .dropdown-menu { display: block; }</style> -->
+	<script src="//p2pool.ovh/js/bootstrap.min.js"></script>
+	<script src="//p2pool.ovh/js/alertify.js"></script>
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 	<style>
-	body {
-		background: #dfe1e2;
-   }
-pre {
-  overflow: auto;
-  word-wrap: normal;
-  white-space: pre;
-}
+		body { background: #dfe1e2; }
+		pre { overflow: auto; word-wrap: normal; white-space: pre; }
    </style>
-
   </head>
-
   <body>
-
-
       <div class="container">
-<nav class="navbar navbar-default" role="navigation">
+		<nav class="navbar navbar-default" role="navigation">
         <div class="container-fluid">
           <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -88,51 +68,61 @@ pre {
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">P2Pool.pl</a>
+            <a class="navbar-brand" href="/">P2Pool.ovh</a>
           </div>
 			<div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-			<li><a href="http://eu.btc.p2pool.pl">Bitcoin</a></li>
-			<li><a href="http://eu.drk.p2pool.pl">DASH</a></li>
-			<li><a href="http://eu.vtc.p2pool.pl">Vertcoin</a></li>
-			<li><a href="http://eu.ftc.p2pool.pl">Feathercoin</a></li>
+			<li><a href="https://p2pool.ovh/btc/">Bitcoin</a></li>
+			<li><a href="https://p2pool.ovh/dash/">DASH</a></li>
+			<li><a href="https://p2pool.ovh/vtc/">Vertcoin</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-              <li><li style="position: relative; display: block; padding: 15px 5px; color: #fff;">BTC: <?=$btc_price;?>$ | DRK <?=$drk_price;?>$ | VTC <?=$vtc_price;?>$ | FTC <?=$ftc_price;?>$</li></li>
+           <?php
+           //   <li><li style="position: relative; display: block; padding: 15px 5px; color: #fff;">BTC: <?=$btc_price;?>$ | DRK <?=$drk_price;?>$ | VTC <?=$vtc_price;?>$ | FTC <?=$ftc_price;?>$</li></li>
+           ?>
             </ul>
           </div><!--/.nav-collapse -->
-		  
         </div><!--/.container-fluid -->
       </nav>
-
       </div><!-- /.container -->
 	
 <div class="container">
 <div class="container">
-<center><img src="/img/logo.png" border="0"></center>  
+<center><img src="/img/logo.png" border="0"></center>
 <div class="row">
+	
 <center><h4>Dear miners, we are glad to see you in our pool.<br/>
 If you have any problems, please write in <a href="https://forum.bits.media/index.php?/topic/15283-p2poolpl-btc-dash-vtc-ftc/" target="_blank">this topic</a>.<br/>
-Do not forget, <lu style="color:#0076A0">mining on p2pools</lu> help avoid 51% network attack.
+Do not forget, <lu style="color:#0076A0">mining on p2pools</lu> help avoid 51% network attack.<br/>
+<font color="red">Dear miners we move to new server and domain P2POOL.OVH, old server work yet.<br/>
+We move P2POOL.PL to new IP too. Please update miner config EU.P2POOL.PL => EU.P2POOL.OVH</font>
 </h4>
+
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-6145440713642996"
+     data-ad-slot="5384393893"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
 </center>
 
 <div class="row">
 
 <div class="col-lg-12">		
-<div class="col-lg-3">
+<div class="col-lg-4">
 <center>
 <h2><img width="112" height="112" src="./img/btc.png"><br/>Bitcoin</h2>
 <div class="panel panel-default">
 <table class="table table-bordered">
 	<tbody>
 	<tr><td>Hashrate</td><td><?=$btc_hashrate;?></td></tr>
-		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.pl:9332</td></tr>
-		<tr><td><img src="/img/us.png" style="margin-bottom:2px;"> Pool</td><td>... </td></tr>
+		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.ovh:9332</td></tr>
 		<tr><td><img src="/img/anon.png" style="margin-bottom:3px;"> Pool</td><td title="same port (9332)">wrwx2dy7jyh32o53.onion</td></tr>
 		<tr><td>Username</td><td>Bitcoin Address</td></tr>
 		<tr><td>Password</td><td>Anything</td></tr>
-		<tr><td>Exchange</td><td><a href="https://btc-e.com/" target="_blank">BTC/USD</a></td></tr>
+		<tr><td>Exchange</td><td><a href="https://poloniex.com/exchange#usdt_btc" target="_blank">BTC/ USD</a></td></tr>
 		<tr><td>Algorithm</td><td>SHA-256</td></tr>
 	</tbody> 
 </table>
@@ -140,19 +130,18 @@ Do not forget, <lu style="color:#0076A0">mining on p2pools</lu> help avoid 51% n
 </center>
 </div>
 
-<div class="col-lg-3">
+<div class="col-lg-4">
 <center>
 <h2><img width="112" height="112" src="./img/dark.png"><br/>DASH</h2>
 <div class="panel panel-default">
 <table class="table table-bordered">
 	<tbody>
 		<tr><td>Hashrate</td><td><?=$drk_hashrate;?></td></tr>
-		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.pl:7903</td></tr>
-		<tr><td><img src="/img/us.png" style="margin-bottom:2px;"> Pool</td><td title="by default.abcd">us.p2pool.pl:7903</td></tr>
+		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.ovh:7903</td></tr>
 		<tr><td><img src="/img/anon.png" style="margin-bottom:3px;"> Pool</td><td title="same port (7903)">wrwx2dy7jyh32o53.onion</td></tr>
 		<tr><td>Username</td><td>DASH Address</td></tr>
 		<tr><td>Password</td><td>Anything</td></tr>
-		<tr><td>Exchange</td><td><a href="https://www.cryptsy.com/markets/view/155" target="_blank">DRK/BTC</a> | <a href="https://www.cryptsy.com/markets/view/213" target="_blank">DRK/USD</a></td></tr>
+		<tr><td>Exchange</td><td><a href="https://poloniex.com/exchange#btc_dash" target="_blank">DASH/ BTC</a></td></tr>
 		<tr><td>Algorithm</td><td>X11</td></tr>
 	</tbody> 
 </table>
@@ -160,40 +149,19 @@ Do not forget, <lu style="color:#0076A0">mining on p2pools</lu> help avoid 51% n
 </center>
 </div>
 
-<div class="col-lg-3">
+<div class="col-lg-4">
 <center>
 <h2><img width="112" height="112" src="./img/vertcoin.128.png"><br/>Vertcoin</h2>
 <div class="panel panel-default">
 <table class="table table-bordered">
 	<tbody>
 	<tr><td>Hashrate</td><td><?=$vtc_hashrate;?></td></tr>
-		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.pl:9171</td></tr>
-		<tr><td><img src="/img/us.png" style="margin-bottom:2px;"> Pool</td><td>... </td></tr>
+		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.ovh:9171</td></tr>
 		<tr><td><img src="/img/anon.png" style="margin-bottom:3px;"> Pool</td><td title="same port (9171)">wrwx2dy7jyh32o53.onion</td></tr>
 		<tr><td>Username</td><td>Vertcoin Address</td></tr>
 		<tr><td>Password</td><td>Anything</td></tr>
-		<tr><td>Exchange</td><td><a href="https://www.cryptsy.com/markets/view/151" target="_blank">VTC/BTC</a></td></tr>
+		<tr><td>Exchange</td><td><a href="https://poloniex.com/exchange#btc_vtc" target="_blank">VTC/ BTC</a></td></tr>
 		<tr><td>Algorithm</td><td>Lyra2REv2</td></tr>
-	</tbody> 
-</table>
-</div>
-</center>
-</div>
-
-<div class="col-lg-3">
-<center>
-<h2><img width="112" height="112" src="./img/feathercoin.png"><br/>Feathercoin</h2>
-<div class="panel panel-default">
-<table class="table table-bordered">
-	<tbody>
-		<tr><td>Hashrate</td><td><?=$ftc_hashrate;?></td></tr>
-		<tr><td><img src="/img/fr.png" style="margin-bottom:2px;"> Pool</td><td>eu.p2pool.pl:19327</td></tr>
-		<tr><td><img src="/img/us.png" style="margin-bottom:2px;"> Pool</td><td>... </td></tr>
-		<tr><td><img src="/img/anon.png" style="margin-bottom:3px;"> Pool</td><td title="same port (19327)">wrwx2dy7jyh32o53.onion</td></tr>
-		<tr><td>Username</td><td>Feathercoin Address</td></tr>
-		<tr><td>Password</td><td>Anything</td></tr>
-		<tr><td>Exchange</td><td><a href="https://www.cryptsy.com/markets/view/5" target="_blank">FTC/BTC</a> | <a href="https://www.cryptsy.com/markets/view/6" target="_blank">FTC/USD</a></td></tr>
-		<tr><td>Algorithm</td><td>NeoScrypt</td></tr>
 	</tbody> 
 </table>
 </div>
@@ -208,49 +176,40 @@ Do not forget, <lu style="color:#0076A0">mining on p2pools</lu> help avoid 51% n
 Please, <u>don`t use an exchange address</u> for payout. And don`t use <lu style="color:#0076A0">no-submit-stale</lu> for sgminer. If you want use <lu style="color:#0076A0">onion p2pool</lu> use this settings (for sgminer).
 <pre>"url" : "socks5:127.0.0.1:9050|stratum+tcp://wrwx2dy7jyh32o53.onion:7903"</pre>
 
-Add two nodes in <lu style="color:#0076A0">example.conf</lu> => when one node offline your miner go to the second node. You can get <a href="http://dash.org.ru/pages/stats.php#p2pool" target="_blank">p2pool list on this page</a>.
+If you have connect problem (sgminer) on vertcoin p2pool => add <lu style="color:#0076A0">no-extranonce</lu> in <lu style="color:#0076A0">example.conf</lu>
+<pre>"pools" : [
+	{
+		"url" : "stratum+tcp://eu.p2pool.ovh:9171",
+		"user" : "VqXQrgz1osVFj4bAedwXmRFU9924R2pwwe",
+		"pass" : "x",
+		"no-extranonce": true
+	}
+]</pre>
+
+You can set <lu style="color:#0076A0">share difficulty</lu> for your miner. It`s simple, just add  <lu style="color:#0076A0">/0+0.00xxxxx</lu> to address, example.<br/>
+Play around and choose the best value for you. Mor info => <a href="http://xpool.net/faq.html" target="_blank">this link</a>.
+<pre>
+		...
+		"user" : "XkB8ySpiqyVHeAXHsNhU83mUJ7Jd3CJaqW/0+0.0016704",
+		...
+</pre>
+
+<!--Add two nodes in <lu style="color:#0076A0">example.conf</lu> => when one node offline your miner go to the second node. You can get full <a href="http://dash.org.ru/pages/stats.php#p2pool" target="_blank">dash p2pool list on this page</a>.
 <pre>
 "pools" : [
 	{
-		"url" : "stratum+tcp://eu.p2pool.pl:7903",
+		"url" : "stratum+tcp://eu.p2pool.ovh:7903",
 		"user" : "XkB8ySpiqyVHeAXHsNhU83mUJ7Jd3CJaqW",
 		"pass" : "x"
 	},
 	{
-		"url" : "stratum+tcp://us.p2pool.pl:7903",
+		"url" : "stratum+tcp://de.p2pool.ovh:7903",
 		"user" : "XkB8ySpiqyVHeAXHsNhU83mUJ7Jd3CJaqW",
 		"pass" : "x"
 	}
-]</pre>
+]</pre> -->
+
 </div>
-</div> 
-	
-	<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-(function (d, w, c) {
-    (w[c] = w[c] || []).push(function() {
-        try {
-            w.yaCounter25288835 = new Ya.Metrika({id:25288835,
-                    webvisor:true,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true});
-        } catch(e) { }
-    });
-
-    var n = d.getElementsByTagName("script")[0],
-        s = d.createElement("script"),
-        f = function () { n.parentNode.insertBefore(s, n); };
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
-
-    if (w.opera == "[object Opera]") {
-        d.addEventListener("DOMContentLoaded", f, false);
-    } else { f(); }
-})(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="//mc.yandex.ru/watch/25288835" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-  </body>
+</div>
+</body>
 </html>
--- status/jsonp.php
+++ status/jsonp.php
@@ -1,79 +1,15 @@
 <?php
-function array_merge_recursive_unique($array1, $array2) {
-  if (empty($array1)) return $array2; //optimize the base case
-  foreach ($array2 as $key => $value) { 
-    if (is_array($value) && is_array(@$array1[$key])) {
-		$value = array_merge_recursive_unique($array1[$key], $value);
-    }
-    $array1[$key] = $value;
-  }
-  return $array1;
-}
+	$hosts = ["http://eu.p2pool.ovh:9332", "http://eu.p2pool.ovh:7903", "http://eu.p2pool.ovh:9171", "http://eu.p2pool.ovh:19327"];
+	if (!isset($_GET['report']) || !isset($_GET['host'])) die("Invalid Request.");
 
-function p2pool_merge($host, $json){
-	global $port, $ctx, $report;
-	$a = file_get_contents("http://$host:".$port[2].$report, false, $ctx);
-	if(!empty($a)){
-		if((stristr($report, '/web/graph_data/local_hash_rate') !== FALSE) || stristr($report, '/web/graph_data/local_dead_hash_rate') !== FALSE){
-			$arr1 = json_decode($a, true);
-			$arr2 = json_decode($json, true);
-			$arr3 = array();
-			foreach ($arr1 as $key => $value) {
-				array_push($arr3, [$value[0], $value[1] + $arr2[$key][1], $value[2] + $arr2[$key][2], $value[3] + $arr2[$key][3]]);
-			}
-			$json = json_encode($arr3);
-		}
-		if($report == '/local_stats'){
-			$arr1 = json_decode($a, true);
-			$arr2 = json_decode($json, true);
-			
-			foreach ($arr1["miner_hash_rates"] as $key => $value) {
-				if(!empty($arr2["miner_hash_rates"][$key])){
-					$arr1["miner_hash_rates"]["$key"] = $value + $arr2["miner_hash_rates"][$key];
-					unset($arr2["miner_hash_rates"][$key]);
-				}
-			}
-
-			$json = json_encode(array_merge_recursive_unique($arr1, $arr2));
-		}
-	}else{
-	die($host.$port[2]);	
-	}
-	return $json;
-}
-
-
-$ctx = stream_context_create(array('http'=>
-	array(
-		'timeout' => 1
-	)
-));
-
-$report = $_GET['report']; 
-$host = $_GET['host'];
-
-$web_url = $host . $report;
-$hosts = ["http://eu.p2pool.pl:9332", "http://eu.p2pool.pl:7903", "http://ru.p2pool.pl:7903", "http://eu.p2pool.pl:9171", 
-		  "http://ru.p2pool.pl:7903", "http://eu.p2pool.pl:9171", "http://ru.p2pool.pl:9171", "http://eu.p2pool.pl:19327", 
-		  "http://ru.p2pool.pl:19327"];
+	$report = $_GET['report']; 
+	$host = $_GET['host'];
+	$web_url = $host . $report;
 	
-if(!in_array($_GET['host'], $hosts)) die("Wrong host.");		
-if (!isset($_GET['report']) || !isset($_GET['host'])) die("Invalid Request.");
-if (file_exists($web_url))	die("Access Denied.");
-if (!filter_var($web_url, FILTER_VALIDATE_URL))	die("Invalid Host1.");
-	
-$port = explode(":", $_GET['host']);
-$json = file_get_contents($web_url);
+	if(!in_array($_GET['host'], $hosts)) die("Wrong host.");	
+	if (file_exists($web_url))	die("Access Denied.");
+	if (!filter_var($web_url, FILTER_VALIDATE_URL))	die("Invalid Host.");
 
-if($port[2] == 7903)
-	$json = p2pool_merge("us.p2pool.pl", $json);
-	
-$json = p2pool_merge("de.p2pool.pl", $json);
-
-if($report == '/local_stats'){
-	$arr = json_decode($json, true);
-	arsort($arr["miner_hash_rates"]);
-	$json = json_encode($arr);
-}
-	
-echo sprintf('%s(%s);', $_GET['callback'], $json);
+	$json = file_get_contents($web_url);
+	echo sprintf('%s(%s);', $_GET['callback'], $json);  
+?>
